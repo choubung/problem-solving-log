ca6fa6509327aeb52ab014bfd35f2caa5ecb12b9
choubung <choubung@sookmyung.ac.kr>
2025-12-03 23:01:59 +0900

    solve(boj/s4): 숫자 카드 2

---
+++ BOJ/silver/lv4/P10816/Main.java
@@ -0,0 +1,32 @@
+package BOJ.silver.lv4.P10816;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < m; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(key, 0)).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
